04011644973ae1a2b5fcc641567a6a39a16e9ec0
Paul I Wilson <paul.i.wilson@ft.com>
2016-03-18 14:25:19 +0000

    homepage first version

--- BUILD/src/js/menu.js
+++ BUILD/src/js/menu.js
@@ -4,42 +4,48 @@ const canvasSelector = '#canvas';
 const menuButtonSelector = '.menu-button a';
 const menuOpenButtonText = 'Menu';
 const menuCloseButtonText = 'Close';
+const menuOpenClass = 'open';
 
-function showMenu(canvas, e){
+function showMenu(canvas, menu, e){
 	e.stopPropagation();
 	canvas.classList.add(offCanvasClass);
-	updateMenuButtonsText(menuCloseButtonText);
+	menu.classList.add(menuOpenClass);
+	updateMenuButtonsText(menuCloseButtonText, 'open');
 }
 
-function hideMenu(canvas){
+function hideMenu(canvas, menu){
 	canvas.classList.remove(offCanvasClass);
-	updateMenuButtonsText(menuOpenButtonText);
+	setTimeout(() => menu.classList.remove(menuOpenClass), 250);
+	updateMenuButtonsText(menuOpenButtonText, 'closed');
 }
 
-function toggleMenu(canvas, e){
-	canvas.classList.contains(offCanvasClass) ? hideMenu(canvas) : showMenu(canvas, e);
+function toggleMenu(canvas, menu, e){
+	e.preventDefault();
+	canvas.classList.contains(offCanvasClass) ? hideMenu(canvas, menu) : showMenu(canvas, menu, e);
 }
 
 function getMenuButtonEls(){
 	return Array.from(document.querySelectorAll(menuButtonSelector));
 }
 
-function updateMenuButtonsText(newText){
-	getMenuButtonEls().forEach(updateMenuButtonText.bind(null, newText));
+function updateMenuButtonsText(newText, state){
+	getMenuButtonEls().forEach(updateMenuButtonTextAndState.bind(null, newText, state));
 }
 
-function updateMenuButtonText(newText, button){
+function updateMenuButtonTextAndState(newText, state, button){
 	button.querySelector('span').textContent = newText;
+	button.setAttribute('data-state', state);
 }
 
-function addEventToMenuButton(canvas, button){
-	button.addEventListener('click', toggleMenu.bind(null, canvas));
+function addEventToMenuButton(canvas, menu, button){
+	button.addEventListener('click', toggleMenu.bind(null, canvas, menu));
 }
 
 export function init(){
 	let canvas = document.querySelector(canvasSelector);
-	getMenuButtonEls().forEach(addEventToMenuButton.bind(null, canvas));
-	canvas.addEventListener('click', hideMenu.bind(null, canvas));
+	let menu = document.querySelector('nav.menu');
+	getMenuButtonEls().forEach(addEventToMenuButton.bind(null, canvas, menu));
+	canvas.addEventListener('click', hideMenu.bind(null, canvas, menu));
 }
 
 
